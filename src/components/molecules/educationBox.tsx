db840f194cfafc7bfa3acb431734469ff597de21
Valentina Cadena Zapata <valentina.cadenaz@udea.edu.co>
2025-05-28 17:48:19 -0500

    Corrección resize en componente educationBox

--- src/components/molecules/educationBox.tsx
+++ src/components/molecules/educationBox.tsx
@@ -11,8 +11,8 @@ interface EducationBoxProps {
 
 const EducationBox = ({ className, title, leftText, rightText, rightTitle, rightDescription }: EducationBoxProps) => {
     return (
-        <div className={`${className} flex p-2 space-x-2 sm:py-4 sm:space-x-4 md:p-4 lg:space-x-16 lg:p-12 xl:space-x-32`}>
-            <div>
+        <div className={`${className} flex p-2 space-x-2 gap-x-2 sm:py-4 sm:space-x-4 md:space-x-8 md:p-4 lg:space-x-16 lg:p-12 xl:space-x-32`}>
+            <div className="min-w-[45%]">
                 <EducationCard title={title} leftText={leftText} rightText={rightText}></EducationCard>
             </div>
             <div className="text-start w-2/3 space-y-5">
